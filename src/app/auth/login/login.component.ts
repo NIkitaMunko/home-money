b03d5b896f8f1ecc19d8cc138a7b1a100ad88c8c
Nikita Munko <nikmoon16@gmail.com>
2025-06-27 12:25:27 +0200

    l6

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SharedModule} from '../../shared/shared.module';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'wfm-login',
-  imports: [],
+  imports: [
+    SharedModule,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    })
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
 
 }
