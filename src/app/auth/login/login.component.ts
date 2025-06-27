10bb53868a8c4dafcb19fe0ba88f204072b468f3
Nikita Munko <nikmoon16@gmail.com>
2025-06-27 13:07:57 +0200

    l7

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {SharedModule} from '../../shared/shared.module';
 import {NgClass, NgIf} from '@angular/common';
+import {UsersService} from '../../shared/services/users.service';
+import {User} from '../../shared/models/user.model';
+import {Message} from '../../shared/models/message.model';
 
 @Component({
   selector: 'wfm-login',
@@ -16,16 +19,40 @@ import {NgClass, NgIf} from '@angular/common';
 export class LoginComponent implements OnInit {
 
   form!: FormGroup;
+  message!: Message;
+
+  constructor(private usersService: UsersService) {
+  }
 
   ngOnInit(): void {
+    this.message = new Message('danger', '');
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
     })
   }
 
+  private showMessage(text: string, type: string = 'danger'): void {
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000)
+  }
+
   onSubmit() {
-    console.log(this.form);
+    const formData = this.form.value;
+    this.usersService.getUserByEmail(formData.email)
+      .subscribe((user: User | undefined) => {
+        if (user) {
+          if (user.password === formData.password) {
+            // todo
+          } else {
+            this.showMessage('Пароль не верный');
+          }
+        } else {
+          this.showMessage('Такого пользователя не существует');
+        }
+      })
   }
 
 }
---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {User} from '../models/user.model';
+import {map, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUserByEmail(email: string): Observable<User | undefined> {
+    return this.http.get<User[]>(`http://localhost:3000/users?email=${email}`)
+      .pipe(
+        map(users => users[0] ? users[0] : undefined)
+      );
+  }
+
+}
