68514d841ba05d3b5a71f36117a24bea5b39904e
Nikita Munko <nikmoon16@gmail.com>
2025-06-30 12:07:02 +0200

    l12

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'wfm-system',
   templateUrl: './system.component.html',
+  imports: [
+    RouterOutlet
+  ]
 })
 export class SystemComponent {
 }
