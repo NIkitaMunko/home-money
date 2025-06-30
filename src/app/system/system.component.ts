ab6beeed040623ceffc0a79d62fc847f7d2c3839
Nikita Munko <nikmoon16@gmail.com>
2025-06-30 12:15:39 +0200

    l13

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'wfm-system',
   templateUrl: './system.component.html',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    SidebarComponent
   ]
 })
 export class SystemComponent {
