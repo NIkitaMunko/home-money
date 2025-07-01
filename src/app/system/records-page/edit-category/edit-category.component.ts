187b8c5fe0c3cd6eed7730062c3bfc22c7fe37c5
Nikita Munko <nikmoon16@gmail.com>
2025-07-01 15:14:25 +0200

    l19

---
+++ src/app/system/records-page/edit-category/edit-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wfm-edit-category',
+  imports: [],
+  templateUrl: './edit-category.component.html',
+  styleUrl: './edit-category.component.scss'
+})
+export class EditCategoryComponent {
+
+}
--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {AddEventComponent} from './add-event/add-event.component';
+import {AddCategoryComponent} from './add-category/add-category.component';
+import {EditCategoryComponent} from './edit-category/edit-category.component';
 
 @Component({
   selector: 'wfm-records-page',
-  imports: [],
+  imports: [
+    AddEventComponent,
+    AddCategoryComponent,
+    EditCategoryComponent
+  ],
   templateUrl: './records-page.component.html',
   styleUrl: './records-page.component.scss'
 })
